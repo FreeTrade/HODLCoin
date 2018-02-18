f6008d30b21ba485e7ebbdc440ce80ab0f72738e
TheTDD <the.tdd@mail.ru>
2018-02-18 17:42:09 +0100

    Rebuild term deposit table and mature coins notification

--- src/qt/overviewpage.h
+++ src/qt/overviewpage.h
@@ -7,12 +7,14 @@
 
 #include "amount.h"
 #include "primitives/transaction.h"
+#include "deposittablemodel.h"
 
 #ifdef ENABLE_WALLET
 #include "wallet/wallet.h" // for COutput
 #endif // ENABLE_WALLET
 
 #include <QWidget>
+#include <QSortFilterProxyModel>
 
 class ClientModel;
 class TransactionFilterProxy;
@@ -20,54 +22,71 @@ class TxViewDelegate;
 class WalletModel;
 
 namespace Ui {
-    class OverviewPage;
+class OverviewPage;
 }
 
 QT_BEGIN_NAMESPACE
 class QModelIndex;
 QT_END_NAMESPACE
 
+class DepositSortFilterProxyModel : public QSortFilterProxyModel
+{
+	Q_OBJECT
+
+public:
+	explicit DepositSortFilterProxyModel(QObject *parent = 0);
+
+protected:
+	// bool lessThan(const QModelIndex &left, const QModelIndex &right) const;
+};
+
 /** Overview ("home") page widget */
 class OverviewPage : public QWidget
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    explicit OverviewPage(QWidget *parent = 0);
-    ~OverviewPage();
+	explicit OverviewPage(QWidget *parent = 0);
+	~OverviewPage();
 
-    void setClientModel(ClientModel *clientModel);
-    void setWalletModel(WalletModel *walletModel);
-    void showOutOfSyncWarning(bool fShow);
+	void setClientModel(ClientModel *clientModel);
+	void setWalletModel(WalletModel *walletModel);
+	void showOutOfSyncWarning(bool fShow);
 
 public Q_SLOTS:
 #ifdef ENABLE_WALLET
-    void setBalance(const CAmount& balance, const CAmount& unconfirmedBalance, const CAmount& immatureBalance,
-                    const CAmount& watchOnlyBalance, const CAmount& watchUnconfBalance, const CAmount& watchImmatureBalance, std::vector<COutput> termDepositInfo);
+	void setBalance(const CAmount& balance, const CAmount& unconfirmedBalance, const CAmount& immatureBalance,
+			const CAmount& watchOnlyBalance, const CAmount& watchUnconfBalance, const CAmount& watchImmatureBalance, std::vector<COutput> termDepositInfo);
 #endif // ENABLE_WALLET
 
 Q_SIGNALS:
-    void transactionClicked(const QModelIndex &index);
+	/** When transaction is clicked */
+	void transactionClicked(const QModelIndex &index);
+	/** Notify that matured coins appeared */
+	void maturedCoinsNotification(int count, int unit, CAmount& amount);
 
 private:
-    Ui::OverviewPage *ui;
-    ClientModel *clientModel;
-    WalletModel *walletModel;
-    CAmount currentBalance;
-    CAmount currentUnconfirmedBalance;
-    CAmount currentImmatureBalance;
-    CAmount currentWatchOnlyBalance;
-    CAmount currentWatchUnconfBalance;
-    CAmount currentWatchImmatureBalance;
+	Ui::OverviewPage *ui;
+	ClientModel *clientModel;
+	WalletModel *walletModel;
+	CAmount currentBalance;
+	CAmount currentUnconfirmedBalance;
+	CAmount currentImmatureBalance;
+	CAmount currentWatchOnlyBalance;
+	CAmount currentWatchUnconfBalance;
+	CAmount currentWatchImmatureBalance;
 
-    TxViewDelegate *txdelegate;
-    TransactionFilterProxy *filter;
+	TxViewDelegate *txdelegate;
+	TransactionFilterProxy *filter;
+
+	DepositSortFilterProxyModel *depositProxyModel;
+	DepositTableModel *depositModel;
 
 private Q_SLOTS:
-    void updateDisplayUnit();
-    void handleTransactionClicked(const QModelIndex &index);
-    void updateAlerts(const QString &warnings);
-    void updateWatchOnlyLabels(bool showWatchOnly);
+	void updateDisplayUnit();
+	void handleTransactionClicked(const QModelIndex &index);
+	void updateAlerts(const QString &warnings);
+	void updateWatchOnlyLabels(bool showWatchOnly);
 };
 
 #endif // BITCOIN_QT_OVERVIEWPAGE_H
