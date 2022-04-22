68fd2ecde1dc5042c4e6737fdf485597feb8fd77
Kees de Wit <github@txbit.io>
2022-04-22 14:49:33 +0200

    Remove invalid money range check

--- src/amount.h
+++ src/amount.h
@@ -18,7 +18,7 @@ static const CAmount CENT = 1000000;
 
 /** No amount larger than this (in satoshi) is valid */
 static const CAmount MAX_MONEY = 600000000 * COIN;
-inline bool MoneyRange(const CAmount& nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
+inline bool MoneyRange(const CAmount& nValue) { return (nValue >= 0); }
 
 /** Type-safe wrapper class to for fee rates
  * (how much to pay based on transaction size)
